089a3d7be52240862ae53e5c59d64b5ab5e3f8cd
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-11-22 15:57:30 -0800

    (styling): altered to table format for show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,10 @@ ColorScheme.create([
 		{ name: 'Orange', colorOne: '#FFFBB9', colorTwo: '#ffb921', colorThree: '#cc5500'},
 		{ name: 'Olive', colorOne: '#5E7F58', colorTwo: '#B64913', colorThree: '#FCe7C8'},
 		{ name: 'Violet', colorOne: '#4C384A', colorTwo: '#726A95', colorThree: '#351F39'},
-		{ name: 'Calm', colorOne: '#BBCBD2', colorTwo: '#495F77', colorThree: '#709Fb0'}
+		{ name: 'Calm', colorOne: '#BBCBD2', colorTwo: '#495F77', colorThree: '#709Fb0'},
+		{ name: 'Relax', colorOne: '#daecf3', colorTwo: '#1aa687', colorThree: '#022d41'},
+		{ name: 'Sunset', colorOne: '#6c5b7b', colorTwo: '#cd6c84', colorThree: '#f67280'},
+		{ name: 'Creative', colorOne: '#4Abdac', colorTwo: '#fc4a1a', colorThree: '#f78733'}
 	]
 )
 
