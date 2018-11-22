1e36cc2a6ea56fbf224cc552eb5ad59d243a8fbd
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-11-22 12:32:43 -0800

    (feature:index) table on index page now shows color_schemes as expected

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ColorScheme.create([
+		{ name: 'Fuego', colorOne: '#FC6600', colorTwo: '#FFBF00', colorThree: '#EF820D'},
+		{ name: 'Orange', colorOne: '#FFFBB9', colorTwo: '#ffb921', colorThree: '#cc5500'},
+		{ name: 'Olive', colorOne: '#5E7F58', colorTwo: '#B64913', colorThree: '#FCe7C8'}
+	]
+)
+
